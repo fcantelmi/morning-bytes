2c95da1f1b2f299e60045f50972e34ae6b4c513b
Frank Cantelmi <frank.cantelmi@gmail.com>
2020-04-10 02:03:57 -0400

    add Puzzle and tests

---
+++ sudoku.py
@@ -0,0 +1,84 @@
+import os
+import re
+
+
+class BacktrackingSolver:
+
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
+        self.solved_puzzle = None
+
+    def possible(self, row, col, value):
+        pass
+
+    def solve(self):
+        pass
+
+
+class Puzzle:
+
+    @classmethod
+    def parse(cls, p):
+        no_whitespace = re.sub(r"\s+", "", p)
+        values = [int(val) if val in '123456789' else None for val in no_whitespace]
+
+        return cls(values)
+
+    def __init__(self, values):
+        self.values = values
+
+    def __str__(self):
+        p = ""
+
+        for row in range(9):
+            row_index = 9 * row
+            split = ["." if it is None else str(it) for it in self.values[row_index:row_index + 9]]
+            p += " ".join(split)
+            p += os.linesep
+
+        return p
+
+    def set_value(self, row, col, value):
+        self.values[row * 9 + col] = value
+
+    def get_value(self, row, col):
+        return self.values[row * 9 + col]
+
+    def box_values(self, row, col):
+        box_row = (row // 3) * 3  # returns 0, 3, or 6
+        box_col = (col // 3) * 3  # returns 0, 3, or 6
+
+        values = [self.get_value(box_row + it_row, box_col + it_col) for it_row in range(3) for it_col in range(3)]
+        return frozenset(values)
+
+    def row_values(self, row):
+        index = 9 * row
+        values = self.values[index:index + 9]
+        return frozenset(values)
+
+    def col_values(self, col):
+        values = self.values[col::9]
+        return frozenset(values)
+
+
+def main():
+    unsolved = """
+        .  .  .  .  .  .  .  1  5
+        .  4  9  .  .  .  .  .  .
+        2  .  .  3  .  1  7  .  .
+        8  .  .  2  .  .  .  9  .
+        .  9  .  .  .  .  .  7  .
+        .  7  .  .  .  6  .  .  1
+        .  .  4  9  .  5  .  .  7
+        .  .  .  .  .  .  5  4  .
+        6  1  .  .  .  .  .  .  .
+    """
+
+    puzzle = Puzzle.parse(unsolved)
+    print(puzzle)
+    solver = BacktrackingSolver(puzzle)
+    solver.solve()
+
+
+if __name__ == '__main__':
+    main()
