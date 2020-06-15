eb2091ae0c9a7bcdd81b4cb817b346e3ca5bc017
Frank Cantelmi <frank.cantelmi@gmail.com>
2020-06-14 22:20:31 -0400

    add perturbation methods for polynomial roots

--- perturbation/roots.py
+++ perturbation/roots.py
@@ -1,7 +1,10 @@
 import sympy as sym
 
-x, x_0, x_1, epsilon = sym.symbols('x x_0 x_1 epsilon')
+x, x_0, x_1, x_2, epsilon = sym.symbols('x x_0 x_1 x_2 epsilon')
 p = (x - 1) * (x + 1) * (x - epsilon)
+
 print(sym.poly(p))
-pert = p.subs(x, x_0 + epsilon * x_1)
-print(sym.collect(sym.expand(pert.series(epsilon, 0, 2)), epsilon))
+
+pert = p.subs(x, x_0 + epsilon * x_1 + epsilon*epsilon*x_2)
+
+print(sym.collect(sym.expand(pert.series(epsilon, 0, 3)), epsilon))
