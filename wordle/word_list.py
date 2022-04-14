da2f15fc19b0621b9d13846de646dcbab449f251
Frank Cantelmi <frank.cantelmi@gmail.com>
2022-04-13 23:56:01 -0400

    refactor find_node and contains_node because they are basically the same

--- wordle/word_list.py
+++ wordle/word_list.py
@@ -1,42 +1,37 @@
-def add_node(word, node):
+def add_node(node, word):
     if len(word) > 0:
         head = word[0]
         tail = word[1:]
         if head not in node:
             node[head] = {}
-        add_node(tail, node[head])
+        add_node(node[head], tail)
 
 
-def contains_node(word, node):
-    if len(word) > 0:
-        head = word[0]
-        tail = word[1:]
-        if head in node:
-            return contains_node(tail, node[head])
-        else:
-            return False
-    else:
-        return True
-
-
-def find_node(prefix, node):
+def find_node(node, prefix):
     if len(prefix) > 0:
         head = prefix[0]
         tail = prefix[1:]
         if head in node:
-            return find_node(tail, node[head])
+            return find_node(node[head], tail)
         else:
             return None
     else:
         return node
 
 
-def startswith_node(words, prefix, node):
+def contains_node(node, word):
+    if find_node(node, word) is None:
+        return False
+    else:
+        return True
+
+
+def startswith_node(node, words, prefix):
     if not node:
         words.add(prefix)
     else:
         for letter, child in node.items():
-            startswith_node(words, prefix + letter, child)
+            startswith_node(child, words, prefix + letter)
 
 
 class WordList:
@@ -48,19 +43,16 @@ class WordList:
         return str(self.trie)
 
     def add(self, word):
-        add_node(word, self.trie)
+        add_node(self.trie, word)
 
     def contains(self, word):
-        return contains_node(word, self.trie)
-
-    def find_node(self, prefix):
-        pass
+        return contains_node(self.trie, word)
 
     def startswith(self, prefix):
         words = set()
-        node = find_node(prefix, self.trie)
+        node = find_node(self.trie, prefix)
         if node:
-            startswith_node(words, prefix, node)
+            startswith_node(node, words, prefix)
 
         return words
 
