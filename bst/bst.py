365a6fb4f5d314ba09b044ce64dc72e220efc801
Frank Cantelmi <frank.cantelmi@gmail.com>
2022-05-16 07:27:44 -0400

    WIP: bst implementation

---
+++ bst/bst.py
@@ -0,0 +1,87 @@
+import collections.abc
+
+
+class BinarySearchTree(collections.abc.MutableSet):
+
+    def __init__(self, iterable=None):
+        self.key = None
+        self.left = None
+        self.right = None
+        if iterable is not None:
+            for key in iterable:
+                self.add(key)
+
+    def __contains__(self, key) -> bool:
+        return key in self.everything()
+
+    def __len__(self) -> int:
+        return len(self.everything())
+
+    def __iter__(self):
+        return iter(self.everything())
+
+    def add(self, key):
+        if self.key is None:
+            self.key = key
+        elif key < self.key:
+            if self.left is None:
+                self.left = BinarySearchTree({key})
+            else:
+                self.left.add(key)
+        elif key > self.key:
+            if self.right is None:
+                self.right = BinarySearchTree({key})
+            else:
+                self.right.add(key)
+        else:
+            pass  # duplicate key
+
+    def discard(self, key):
+        pass
+
+    def max(self):
+        if self.right is None:
+            return self.key
+        else:
+            return self.right.max()
+
+    def collect(self, keys):
+        if self.left:
+            self.left.collect(keys)
+
+        keys.append(self.key)
+
+        if self.right:
+            self.right.collect(keys)
+
+    def everything(self):
+        keys = []
+        self.collect(keys)
+        return keys
+
+
+def main():
+    root = BinarySearchTree()
+    root.add('cat')
+    root.add('bunny')
+    root.add('dog')
+    root.add('dog')
+    root.add('parrot')
+    root.add('monkey')
+    print(root.everything())
+    print(root.max())
+
+    for key in root:
+        print(key)
+
+    print(len(root))
+
+    ts = BinarySearchTree()
+    ts.add(0)
+    ts.add(-1)
+    ts.add(1)
+    print(ts.everything())
+
+
+if __name__ == '__main__':
+    main()
